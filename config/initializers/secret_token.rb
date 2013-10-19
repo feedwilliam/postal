d754e20e4011faaebdbc46c864443147504a4609
William Kong <william_kong@yahoo.com>
2013-10-19 10:50:44 -0700

    Added all the files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postal::Application.config.secret_token = 'b000e40a51740bf95b43bc8575c8e8d5f6cbc6d130b6bd5ebe7f8cb47b84afca11fe3a345f2ae6a9daf8d18767cf53ec758a5a30b5fe713ecae744533888537d'
